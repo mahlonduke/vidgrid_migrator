947d2c5ef03920618e207389ec51df2d28c31d32
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-05-21 17:35:41 -0700

    Added downloader from 23

---
+++ api_config.py
@@ -0,0 +1,14 @@
+# TwentyThree Credentials
+ttKey = '62960280-WIYuQgGdIzbMbMB4Acpz'
+ttConsumerSecret = 'rDU3StZRcdbpryS2YQ1UR5qUKpqLYrFW6A4rxOYnCOSx1iR1rh'
+ttToken = '86846-fP3BIu4qfY4ZtC8FMrZD'
+ttTokenSecret = 'JYFAL9ftkRUkcmIGVjeEGhmcAigulonq04xSMYa8EawZu6PfUq'
+ttPermissionLevel = 'admin'
+
+
+# VidGrid Credentials
+vidGridKey = 'vg_YMwafKdn17TpyOZdB9PL'
+vidGridSecret = '5INLYxMCNiurfjQBASDppTlDBIewUTUFGBbhkW4c'
+
+
+# Vimeo Credentials
