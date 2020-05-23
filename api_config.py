df57cc0338c1769f5d1418febc123d52ec65193d
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-05-22 19:00:35 -0700

    Changed 23 API's auth format from bearer token to OAuth1

--- api_config.py
+++ api_config.py
@@ -1,9 +1,11 @@
 # TwentyThree Credentials
-ttKey = '62960280-WIYuQgGdIzbMbMB4Acpz'
-ttConsumerSecret = 'rDU3StZRcdbpryS2YQ1UR5qUKpqLYrFW6A4rxOYnCOSx1iR1rh'
-ttToken = '86846-fP3BIu4qfY4ZtC8FMrZD'
-ttTokenSecret = 'JYFAL9ftkRUkcmIGVjeEGhmcAigulonq04xSMYa8EawZu6PfUq'
-ttPermissionLevel = 'admin'
+## accelo-marketing account
+baseURL = 'http://accelo-marketing.videomarketingplatform.co/'
+ttKey = '63002693-hSuAxO4hmvdPxNx45LDp'
+ttConsumerSecret = '3z8YMCWOt6LvSc5hvHTEAgViH0WtmhcLAyElbAxnTNbLgEAMVi'
+ttToken = '86860-4buhkstkne169su0uZBN'
+ttTokenSecret = 'UPUiGWJmrmVSDuulfzmmG9EiYvK0PZxHtywWEiPoY6loRouY94'
+ttPermissionLevel = 'super'
 
 
 # VidGrid Credentials
@@ -15,3 +17,14 @@ vidGridSecret = '5INLYxMCNiurfjQBASDppTlDBIewUTUFGBbhkW4c'
 vimClient = '33fb0f8e5db5ea25ee8442a2afbaaeaa27cda0ba'
 vimToken = 'b4d6bc634d86189765fa0fc72005e90b'
 vimSecret = 'T9nOirRhfSOkCSu/2823D1GYQhK4uKR5v3pExDyU9JbX/gLLOeMuNG8kHSG6ZCmhs2+LWb6EF60L6arQT1kOpB3Kxc09FjS5sPHumxVCVdOWEvaAcV4JxNWfXr7fHBIN'
+
+
+
+
+
+# Consumer key 63002693-hSuAxO4hmvdPxNx45LDp
+# Consumer secret 3z8YMCWOt6LvSc5hvHTEAgViH0WtmhcLAyElbAxnTNbLgEAMVi
+# Access token 86860-4buhkstkne169su0uZBN
+# Access token secret UPUiGWJmrmVSDuulfzmmG9EiYvK0PZxHtywWEiPoY6loRouY94
+# Permission level super
+
