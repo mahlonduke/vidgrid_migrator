4d688cc2ca85a9772fd0f6fad92a4030f1e0c92e
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2020-06-04 10:07:35 -0700

    Created sample api_config.py file

---
+++ api_config.py
@@ -0,0 +1,12 @@
+# TwentyThree Credentials
+baseURL = 'https://your-23-URL.com'
+ttConsumerKey = 'Your 23 app's consumer key'
+ttConsumerSecret = 'Your 23 app's consumer secret'
+ttAccessToken = 'Your 23 app's access token'
+ttAccessTokenSecret = 'Your 23 app's token secret'
+
+
+# Vimeo Credentials
+vimClientID = 'Your Vimeo app's client ID'
+vimSecret = 'Your Vimeo app's client secret'
+vimAccessToken = 'Your Vimeo app's access token'
